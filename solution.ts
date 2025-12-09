1b657d7507cde42d6908e354fd1c3cb2664ed3c0
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-12-10 01:32:41 +0600

    problem 6 done

--- solution.ts
+++ solution.ts
@@ -57,3 +57,24 @@ const books = [
 ];
 
 // Problem-5
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+}[];
+
+const filterActiveUsers = (users: User): User => {
+  return users.filter((user) => {
+    return user.isActive;
+  });
+};
+
+const users = [
+  { id: 1, name: "Rakib", email: "rakib@example.com", isActive: true },
+  { id: 2, name: "Asha", email: "asha@example.com", isActive: false },
+  { id: 3, name: "Rumi", email: "rumi@example.com", isActive: true },
+];
+
+// Problem-6
